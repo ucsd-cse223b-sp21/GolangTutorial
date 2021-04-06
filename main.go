cba63d9e93769f4fa5cc1e8953acabbb5325a014
wantonsolutions <stewbertgrant@gmail.com>
2021-04-06 08:30:43 -0700

    parse command line arguments

--- main.go
+++ main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	client bool
+	server bool
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Entry function main")
+
+	if len(os.Args) != 2 {
+		log.Fatal("Only pass one command line argument to this program")
+	}
+
+	val := os.Args[1]
+
+	if val == "client" {
+		client = true
+	} else if val == "server" {
+		server = true
+	}
+
+	if client {
+		run_client()
+	} else if server {
+		run_server()
+	}
+}
+
+func run_client(){
+	log.Println("Starting the client")
+}
+
+func run_server() {
+	log.Println("Starting the server")
 }
