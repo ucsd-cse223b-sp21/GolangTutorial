b7d09fa662cf5b53afde8b696676a963c1966649
wantonsolutions <stewbertgrant@gmail.com>
2021-04-06 08:42:44 -0700

    rpc defined

--- main.go
+++ main.go
@@ -11,6 +11,28 @@ var (
 	server bool
 )
 
+const (
+	RESPONSE_OK = 1
+	RESPONSE_BAD = 2
+	RESPONSE_MAGIC = 3
+)
+
+type ScoreServer int
+
+type Message struct {
+	User string
+	Contents string
+}
+
+type MessageResponse struct {
+	ResponseCode int
+}
+
+func (ss *ScoreServer) PutScore(m Message, mr *MessageResponse) error {
+	log.Println("Made it to the Put Score")
+	return nil
+}
+
 func main() {
 	fmt.Println("Entry function main")
 
